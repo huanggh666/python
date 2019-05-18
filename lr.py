4ea23c02aa0375aa7182c8576ba5ad0456b5420e
海角天涯 <33412859+ihuanggh@users.noreply.github.com>
2019-05-18 11:37:39 +0800

    upload

---
+++ lr.py
@@ -0,0 +1,9 @@
+def lr_poly(base_lr, iter, max_iter, power):
+    return base_lr*((1-float(iter)/max_iter)**(power))
+
+
+def adjust_learning_rate(optimizer, i_iter):
+    lr = lr_poly(args.learning_rate, i_iter, args.num_steps, args.power)
+    optimizer.param_groups[0]['lr'] = lr
+    if len(optimizer.param_groups) > 1 :
+        optimizer.param_groups[1]['lr'] = lr * 10
